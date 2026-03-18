ea3bdb9b83e62e0a0ae078628fd60ce3a9434c05
Dusharg <gargdushar@gmail.com>
2026-03-18 18:23:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+ 
+  return{
+  <div>
+  <h1>Welcome to my App</h1>
+  <form className='form' centered ="true">
+    <label htmlFor="name">Name:</label>
+    <input type="text" id="name" name="name" required />
+    <button type="submit">Submit</button>
+  </form>
+  </div>
+  }
+}
+
+export default App
